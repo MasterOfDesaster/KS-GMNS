934ed223f511637b3b026f1ccd7b66a02e81ddac
gabriel <gabriel1997@hotmail.de>
2015-02-09 15:07:44 -0800

    TCP Layer Update

--- src/experiments/experiment1/stack/fsm/State.groovy
+++ src/experiments/experiment1/stack/fsm/State.groovy
@@ -7,29 +7,44 @@ class State {
     /** Leerlauf */
     static final int S_IDLE = 100
 
+    /** Auf SYN wartend */
+    static final int S_WAIT_SYN = 105
+
     /** Auf SYN+ACK wartend */
     static final int S_WAIT_SYN_ACK = 110
 
-    /** SYN gesendet -passiv- */
-    static final int S_SEND_SYN_ACK = 115
+    /**  Auf SYN+ACK+ACK warten*/
+    static final int S_WAIT_SYN_ACK_ACK = 115
 
     /** Wartend */
     static final int S_READY = 120
 
+    /** Senden von SYN-ACK*/
+    static final int S_SEND_SYN_ACK = 130
+
     /** Senden von ACK als Abschluß der Verbindungseröffnung */
     static final int S_SEND_SYN_ACK_ACK = 180
 
-    /** auf SYN+ACK+ACK wartend -passiv- */
-    static final int S_WAIT_SYN_ACK_ACK = 185
+    /** Empfangen von SYN ACK ACK als Abschluss der Verbindungseröffnung*/
+    static final int S_RCVD_SYN_ACK_ACK = 185
 
     /** Auf FIN+ACK wartend */
     static final int S_WAIT_FIN_ACK = 190
 
+    /** Senden des FIN-ACK's*/
+    static final int S_SEND_FIN_ACK = 195
+
     /** Senden von ACK als Abschluß der Verbindungbeendigung */
     static final int S_SEND_FIN_ACK_ACK = 200
 
+    /** warten auf FIN ACK ACK*/
+    static final int S_WAIT_FIN_ACK_ACK = 205
+
     /** Daten wurden empfangen */
     static final int S_RCVD_DATA = 210
+
+    /** FIN ACK ACK wurde empfangen*/
+    static final int S_RCVD_FIN_ACK_ACK = 215
 
     /** ACK wurde empfangen */
     static final int S_RCVD_ACK = 220
@@ -37,23 +52,10 @@ class State {
     /** SYN zur Verbindungseröffnung wird gesendet */
     static final int S_SEND_SYN = 230
 
-    //auf SYN wartend -passiv-
-    static final int S_WAIT_SYN = 235
-
     /** SYN zur Verbindungsbeendigung wird gesendet */
     static final int S_SEND_FIN = 240
 
-    /** Fin wird bestätigt */
-    static final int S_SEND_FIN_ACK = 245
-
-    /** Auf Bestätigung des Fins warten */
-    static final int S_WAIT_FIN_ACK_ACK = 255
-
     /** Daten werden gesendet */
     static final int S_SEND_DATA = 250
-
-    static final int S_RCVD_SYN_ACK_ACK = 260
-
-    static final int S_RCVD_FIN_ACK_ACK = 270
 
 }
